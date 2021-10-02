4015e45e8b17320ef2aae49046fc6d55e860ce4c
James Hicks <jhicks2306@gmail.com>
2021-10-02 10:14:00 +0100

    Add foreach.js

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,16 @@
 // Reverses a string.
 function reverse(string) {
-  return string.split("").reverse().join("");
+  return Array.from(string).reverse().join("");
 }
 
 // Returns true for a palindrome, false otherwise
 function palindrome(string) {
   let processedContent = string.toLowerCase();
   return processedContent === reverse(processedContent);
+}
+
+//Returns parts of an email.
+function emailParts(email) {
+  let processedContent = email.toLowerCase();
+  return processedContent.split(/(?=@)/g)
 }
