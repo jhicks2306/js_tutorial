dfd3ec1635e8995c08cf2231dbb5599e66e0eb12
James Hicks <jhicks2306@gmail.com>
2021-10-19 20:37:21 +0100

    Add form to palindrome.html

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let Phrase = require("palindrome-detector-jhicks2306");
 
-let string = prompt("Please enter a string for palindrome testing");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing");
+  let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(phrase.content + " is a palindrome!");
-} else {
-  alert(phrase.content + " is not a palindrome.");
+  if (phrase.palindrome()) {
+    alert(phrase.content + " is a palindrome!");
+  } else {
+    alert(phrase.content + " is not a palindrome.");
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.querySelector("#palindromeTester");
+  form.addEventListener("submit", function() {
+    palindromeTester();
+    })
+});
